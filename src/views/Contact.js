1f71e3276fecaff96fa57860d733ff9053c6e2e2
Mahendran <info@mahendran.net>
2021-08-01 18:40:02 +0530

    gallery added

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,7 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Fragment, useState, useEffect} from 'react';
+ import { Formik, Form, Field, ErrorMessage , useField} from 'formik';
+ import * as Yup from 'yup';
+ import axios from 'axios';
+
+
+  const ContactSchema = Yup.object().shape({
+   name: Yup.string().required('Name is required'),
+   subject: Yup.string().required('Subject is required'),
+   email: Yup.string().email('Invalid email').required('Email is required'),
+   comments: Yup.string().required('Comments is required'),
+ });
+
 
 const Contact = props => {
+
+   const [formData, setFormData] = useState({
+      name: "",
+      subject: "",
+      email: "",
+      comments: ""
+   })
+
+   const [submitted, setSubmitted] = useState(false);
+   const [submitError, setSubmitError] = useState(false);
+
+   const handleFormSubmit = e => {
+      axios({
+         method: 'post',
+         url: `index.php`,
+         headers: { 'content-type': 'application/json' },
+         data: formData
+      })
+         .then(result => {
+            console.com(result);
+            setSubmitted(true);
+         })
+         .catch(error => setSubmitError(true) );
+   };
     return (
         <section id="contact" class="contact section-bg ">
             <div class="container">
@@ -10,36 +45,64 @@ const Contact = props => {
                   <p>In case of any query feel free to reach us at any of our social media handles, you can find links to those on the left. A discussion forum for all defence aspirants is under development where you can discuss over different topics related to selection in the forces with other aspirants and get your answers. </p>
                </div>
                <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-                  <form action="sendmail.php" method="post" role="form" class="php-email-form">
+               <Formik
+               initialValues={{
+                  name: "",
+                  subject: "",
+                  email: "",
+                  comments: ""
+               }}
+               validationSchema={Yup.object({
+                  name: Yup.string()
+                     .required("Name is required"),
+                  subject: Yup.string()
+                     .required("Subject is required"),
+                  email: Yup.string()
+                     .email("Invalid email addresss")
+                     .required("Email is required"),
+               })}
+               onSubmit={(values, { setSubmitting }) => {
+                     console.log(values);
+                     setSubmitting(false);
+                     setFormData({
+                        name: values.name,
+                        subject: values.subject,
+                        email: values.email,
+                        comments: values.comments
+                     });
+                     handleFormSubmit();
+               }}
+               >
+                  <Form class="php-email-form">
                      <div class="form-row">
                         <div class="form-group col-md-6">
                            <label for="name">Your Name</label>
-                           <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars"/>
-                           <div class="validate"></div>
+                           <Field name="name" className="form-control"/>
+                           <ErrorMessage name="name" component="span" className="error" />
                         </div>
                         <div class="form-group col-md-6">
-                           <label for="name">Your Email</label>
-                           <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email"/>
-                           <div class="validate"></div>
+                           <label for="email">Your Email</label>
+                           <Field name="email" className="form-control"/>
+                           <ErrorMessage name="email" component="span" className="error" />
                         </div>
                      </div>
                      <div class="form-group">
-                        <label for="name">Subject</label>
-                        <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject"/>
-                        <div class="validate"></div>
+                        <label for="subject">Subject</label>
+                        <Field name="subject" className="form-control"/>
+                        <ErrorMessage name="subject"  component="span" className="error"/>
                      </div>
                      <div class="form-group">
-                        <label for="name">Message</label>
-                        <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
-                        <div class="validate"></div>
+                        <label for="comments">Message</label>
+                        <textarea class="form-control" name="comments" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
                      </div>
                      <div class="mb-3">
                         <div class="loading">Loading</div>
-                        <div class="error-message">Enter Valid Mail address. </div>
-                        <div class="sent-message">Your message has been sent. Thank you!</div>
                      </div>
-                     <div class="text-center"><button type="submit">Send Message</button></div>
-                  </form>
+                     <button type="submit">Send Message</button>
+                     <div>{submitted && "Form Submitted, Thank you"}</div>
+                     <div>{submitError && "Something went wrong, Try Again"}</div>
+                  </Form>
+                  </Formik>
                </div>
             </div>
          </section>
--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,68 +1,87 @@
 import React from 'react';
 import { Fancybox } from "@fancyapps/ui";
-import Image1 from "../images/img1.jpeg"
-import Image2 from "../images/img2.jpeg"
-import Image3 from "../images/img3.jpeg"
-import Image4 from "../images/img4.jpeg"
+import Image1 from "../images/banner/img1.jpg"
+import Image2 from "../images/banner/img2.jpg"
+import Image3 from "../images/banner/img3.jpg"
+import Image4 from "../images/banner/img4.jpg"
+import Image5 from "../images/banner/img5.jpg"
+import Image6 from "../images/banner/img6.jpg"
+import Image7 from "../images/banner/img7.jpg"
+import Image8 from "../images/banner/img8.jpg"
+import Image9 from "../images/banner/img9.jpg"
+import Image10 from "../images/banner/img10.jpg"
+import Image11 from "../images/banner/img11.jpg"
+import Image12 from "../images/banner/img12.jpg"
+import Image13 from "../images/banner/img13.jpg"
+import Image14 from "../images/banner/img14.jpg"
+import Image15 from "../images/banner/img15.jpg"
 
 
 const Gallery = () => {
    let images = [
       {
          name: "image1",
-         path: "../images/banner/img1.jpg"
+         path: Image1
       },
        {
          name: "image2",
-         path: "../images/banner/img2.jpg"
+         path: Image2
       },
        {
          name: "image3",
-         path: "../images/banner/img3.jpg"
+         path: Image3
       },
        {
          name: "image4",
-         path: "../images/banner/img4.jpg"
+         path: Image4
       },
        {
          name: "image5",
-         path: "../images/banner/img5.jpg"
+         path: Image5
       },
        {
          name: "image6",
-         path: "../images/banner/img6.jpg"
+         path: Image6
       },
        {
          name: "image7",
-         path: "../images/banner/img7.jpg"
+         path: Image7
       },
        {
          name: "image8",
-         path: "../images/banner/img8.jpg"
+         path: Image8
       },
        {
          name: "image9",
-         path: "../images/banner/img9.jpg"
+         path: Image9
       },
        {
          name: "image10",
-         path: "../images/banner/img10.jpg"
+         path: Image10
       },
        {
          name: "image11",
-         path: "../images/banner/img11.jpg"
+         path: Image11
       },
        {
          name: "image12",
-         path: "../images/banner/img12.jpg"
+         path: Image12
       },
        {
          name: "image13",
-         path: "../images/banner/img13.jpg"
+         path: Image13
+      },
+      {
+         name: "image14",
+         path: Image14
+      },
+       {
+         name: "image15",
+         path: Image15
       }
    ]
 
-   const ImageGal = () => {
+   const Gallery = () => {
       return images.map(img => 
          <div class="item col-sm-6 col-md-4 mb-3">
             <a href={img.path} class="fancybox" data-fancybox="gallery1">
@@ -78,7 +97,7 @@ const Gallery = () => {
                <h2>Gallery</h2>
                <div className="container">
                <div class="row mt-4">
-                  <ImageGal />
+                  <Gallery />
                </div>
             </div>
             </div>
